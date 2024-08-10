70407a9f232906127a6b4da4ac01cc3e5fccb508
nurzhank <asapnrz@gmail.com>
2024-08-10 13:46:57 +0500

    upd

--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models):
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
